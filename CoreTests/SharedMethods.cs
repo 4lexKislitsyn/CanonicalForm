d522e74665ebdbbce3406ba0e30b11e19f5174fe
Alexander Kislitsyn <4lex.kislitsyn@gmail.com>
2020-05-30 14:50:35 +0300

    Documentation of shared methods and fields in test's project.

--- CoreTests/SharedMethods.cs
+++ CoreTests/SharedMethods.cs
@@ -7,9 +7,18 @@ namespace CoreTests
 {
     internal static class SharedMethods
     {
+        /// <summary>
+        /// Instance of <see cref="DefaultObjectPoolProvider"/>
+        /// </summary>
         internal static readonly ObjectPoolProvider PoolProvider = new DefaultObjectPoolProvider();
+        /// <summary>
+        /// Policy to create instance of <see cref="ObjectPool{T}"/> for <see cref="StringBuilder"/>.
+        /// </summary>
         internal static readonly StringBuilderPooledObjectPolicy StringBuilderPolicy = new StringBuilderPooledObjectPolicy();
-
+        /// <summary>
+        /// Create instance of <see cref="ObjectPool{T}"/> using <see cref="PoolProvider"/> and <see cref="StringBuilderPolicy"/>.
+        /// </summary>
+        /// <returns></returns>
         public static ObjectPool<StringBuilder> CreatePool() => PoolProvider.Create(StringBuilderPolicy);
     }
 }
